5ad56d89ac0e44c0b5dffd39d71d0528c1f99212
Godflay <gg.godfreygabriel@gmail.com>
2024-06-27 15:56:59 -0400

    remove dev console for distro

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.title = "Shooting Gallery project"       --the title of the window the game is in (string)
     t.version = "11.5"          -- the love 2d version the game is built on (string)
-    t.console = true            -- attach a console (bool, windows only, dev mode)
+    t.console = false           -- attach a console (bool, windows only, dev mode)
     t.window.vsync = 0          -- disable or enable vsync
 end
